6ce814da8fba19c8ec6a414569cb87b7024e59bb
jebitok-dev <sharonkosgei4@gmail.com>
2020-08-21 21:53:46 +0300

    ecommerce site

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      meaningOfLife: 47
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.meaningOfLife}
+          </p>
+          <button>
+            Update State
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
