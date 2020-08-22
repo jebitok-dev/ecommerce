dfcad37a6586cf9dc261eef48310a41caf6e12ca
jebitok-dev <sharonkosgei4@gmail.com>
2020-08-22 18:53:40 +0300

    editted jsx

--- src/App.js
+++ src/App.js
@@ -8,20 +8,28 @@ class App extends React.Component {
     this.state = {
       meaningOfLife: 47
     }
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.meaningOfLife}
-          </p>
-          <button>
-            Update State
-          </button>
-        </header>
-      </div>
-    );
   }
+
+    handleClick = () => {
+      this.setState({meaningOfLife: this.state.meaningOfLife + 1})
+    }
+    render() {
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                {this.state.meaningOfLife}
+              </p>
+              <button
+                onClick={this.handleClick}
+              >
+                Update State
+              </button>
+            </header>
+          </div>
+        );
+    }
 }
 
 export default App;
