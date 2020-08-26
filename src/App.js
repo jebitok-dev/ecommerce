cf5152c310421a7cc97204302cdc878371f221db
jebitok-dev <sharonkosgei4@gmail.com>
2020-08-26 11:58:52 +0300

    added react-router dom & routes wrapped on switch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
+//route takes a number of components (exact- false or true/path-localhost:3000/, /component)
 function App() {
         return (
           <div>
-            <HomePage />
+          <Switch>
+            <Route exact path='/' component={HomePage} /> 
+            <Route path='/hats' component={HatsPage} />
+          </Switch>
           </div>
         );
 }
