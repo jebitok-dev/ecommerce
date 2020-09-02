8513073d67e2471b963fe5d9395ed36ab62e14be
jebitok-dev <sharonkosgei4@gmail.com>
2020-09-02 18:41:47 +0300

    adding svg

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { ReactComponent as Logo } from '../../assets/crown.svg.svg';
 
 import './header.styles.scss';
 
 const Header = () => (
     <div className='header'>
         <Link className='logo-container' to='/' >
-            <Logo className='log' />
-            SHOP
+            <Logo className='logo' />
         </Link>
         <div className='options'>
+            <Link className='option' to='/shop' >
+                SHOP
+            </Link>
             <Link className='option' to='/shop' >
                 CONTACT
             </Link>
