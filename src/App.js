4d936a495567b3ce49b9c4ef24d01b805cae6fdb
jebitok-dev <sharonkosgei4@gmail.com>
2020-09-03 12:02:56 +0300

    crreated the sign in & sign up page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
 import ShopPage from './pages/shoppage/shop.component';
 import Header from './components/header/header.component';
+import SignAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 //switch gives control of our code
 //route takes a number of components (exact- false or true/path-localhost:3000/, /component)
