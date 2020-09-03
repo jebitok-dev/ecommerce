77f03224345d6d70ffd2363749af15d5d399a551
jebitok-dev <sharonkosgei4@gmail.com>
2020-09-03 13:12:28 +0300

    adding sign in form

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Switch> 
             <Route exact path='/' component={HomePage} /> 
             <Route path='/shop' component={ShopPage} />
+            <Route path='/signin' component={SignAndSignUpPage} />
           </Switch>
           </div>
         );
