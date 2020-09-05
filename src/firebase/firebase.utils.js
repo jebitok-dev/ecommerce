47db5db44e6ba3ddb1368ca3a5ed3ab102ba2fef
jebitok-dev <sharonkosgei4@gmail.com>
2020-09-05 23:07:58 +0300

    added async await on handle submit

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,7 +16,7 @@ const config = {
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
-  export const firestore = firebase();
+  export const firestore = firebase.firestore();
 
   //setting up google authentication
 const provider = new firebase.auth.GoogleAuthProvider();
